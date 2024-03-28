85e3f97bf347cc9244fdfdf027e6311b8fea0c75
AlexL <as.lyashenko@ined.ru>
2024-03-28 19:46:10 +0300

    Коммит

--- src/assets/sw.js
+++ src/assets/sw.js
@@ -1,5 +1,5 @@
 "use strict";
 importScripts("sw-toolbox.js");
-toolbox.precache(["index.html","style/style.css"]);
-toolbox.router.get("/images/*", toolbox.cacheFirst);
+toolbox.precache(["index.html","main.css"]);
+toolbox.router.get("/icons/*", toolbox.cacheFirst);
 toolbox.router.get("/*", toolbox.networkFirst, { networkTimeoutSeconds: 5});
